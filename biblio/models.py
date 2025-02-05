5d44e5fb2229fa0dde5ef5e00e179a68a8826457
Enric Mieza <emieza@xtec.cat>
2025-02-05 21:15:12 +0100

    BasicAuth i TokenAuth

--- biblio/models.py
+++ biblio/models.py
@@ -109,7 +109,8 @@ class Cicle(models.Model):
 class Usuari(AbstractUser):
     centre = models.ForeignKey(Centre,on_delete=models.SET_NULL,null=True,blank=True)
     cicle = models.ForeignKey(Cicle,on_delete=models.SET_NULL,null=True,blank=True)
-    imatge = models.ImageField(upload_to='usuaris/',null=True,blank=True) 
+    imatge = models.ImageField(upload_to='usuaris/',null=True,blank=True)
+    auth_token = models.CharField(max_length=32,blank=True,null=True)
 
 class Reserva(models.Model):
     class Meta:
