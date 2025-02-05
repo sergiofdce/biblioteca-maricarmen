45d0aa25410110989ec10bf70248c41e7a185201
Enric Mieza <emieza@xtec.cat>
2025-02-05 21:15:44 +0100

    ninja api

---
+++ biblio/api.py
@@ -0,0 +1,60 @@
+from django.contrib.auth import authenticate
+from ninja import NinjaAPI, Schema
+from ninja.security import HttpBasicAuth, HttpBearer
+from .models import *
+from typing import List
+import secrets
+
+api = NinjaAPI()
+
+
+# Autenticació bàsica
+class BasicAuth(HttpBasicAuth):
+    def authenticate(self, request, username, password):
+        user = authenticate(username=username, password=password)
+        if user:
+            # Genera un token simple
+            token = secrets.token_hex(16)
+            user.auth_token = token
+            user.save()
+            return token
+        return None
+
+# Autenticació per Token Bearer
+class AuthBearer(HttpBearer):
+    def authenticate(self, request, token):
+        try:
+            user = Usuari.objects.get(auth_token=token)
+            return user
+        except Usuari.DoesNotExist:
+            return None
+
+# Endpoint per obtenir un token
+@api.post("/token/", auth=BasicAuth())
+def obtenir_token(request):
+    return {"token": request.auth}
+
+class LlibreOut(Schema):
+    id: int
+    titol: str
+    editorial: str
+    #ISBN: str
+
+class LlibreIn(Schema):
+    titol: str
+    editorial: str
+
+
+@api.get("/llibres", response=List[LlibreOut], auth=AuthBearer())
+def get_llibres(request):
+    qs = Llibre.objects.all()
+    return qs
+
+@api.post("/llibres")
+def post_llibres(request, payload: LlibreIn):
+    llibre = Llibre.objects.create(**payload.dict())
+    return {
+        "id": llibre.id,
+        "titol": llibre.titol
+    }
+
