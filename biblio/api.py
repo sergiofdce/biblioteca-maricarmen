051ee871a5712bcf5e618990417fbaef9979d4a8
Enric Mieza <emieza@xtec.cat>
2025-02-19 20:56:22 +0100

    exemplars

--- biblio/api.py
+++ biblio/api.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate
 from ninja import NinjaAPI, Schema
 from ninja.security import HttpBasicAuth, HttpBearer
 from .models import *
-from typing import List, Optional
+from typing import List, Optional, Union, Literal
 import secrets
 
 api = NinjaAPI()
@@ -35,11 +35,22 @@ class AuthBearer(HttpBearer):
 def obtenir_token(request):
     return {"token": request.auth}
 
-class LlibreOut(Schema):
+class CatalegOut(Schema):
     id: int
     titol: str
+    autor: Optional[str]
+
+class LlibreOut(CatalegOut):
     editorial: Optional[str]
     ISBN: Optional[str]
+
+class ExemplarOut(Schema):
+    id: int
+    registre: str
+    exclos_prestec: bool
+    baixa: bool
+    cataleg: Union[LlibreOut,CatalegOut]
+    tipus: str
 
 class LlibreIn(Schema):
     titol: str
@@ -61,3 +72,44 @@ def post_llibres(request, payload: LlibreIn):
         "titol": llibre.titol
     }
 
+@api.get("/exemplars", response=List[ExemplarOut])
+@api.get("/exemplars/", response=List[ExemplarOut])
+def get_exemplars(request):
+    # carreguem objectes amb els proxy models relacionats exactes
+    exemplars = Exemplar.objects.select_related(
+        "cataleg__llibre",
+        "cataleg__revista",
+        "cataleg__cd",
+        "cataleg__dvd",
+        "cataleg__br",
+        "cataleg__dispositiu",
+    ).all()
+    result = []
+
+    for exemplar in exemplars:
+        cataleg_instance = exemplar.cataleg
+
+        # Determinar el tipus de l'objecte Cataleg
+        if hasattr(cataleg_instance, "llibre"):
+            cataleg_schema = LlibreOut.from_orm(cataleg_instance.llibre)
+            tipus = "llibre"
+        #elif hasattr(cataleg_instance, "dispositiu"):
+        #    cataleg_schema = LlibreOut.from_orm(cataleg_instance.dispositiu)
+        # TODO: afegir altres esquemes
+        else:
+            cataleg_schema = CatalegOut.from_orm(cataleg_instance)
+            tipus = "indefinit"
+
+        # Afegir l'Exemplar amb el Cataleg serialitzat
+        result.append(
+            ExemplarOut(
+                id=exemplar.id,
+                registre=exemplar.registre,
+                exclos_prestec=exemplar.exclos_prestec,
+                baixa=exemplar.baixa,
+                cataleg=cataleg_schema,
+                tipus=tipus,
+            )
+        )
+
+    return result
